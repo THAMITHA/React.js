8f51e0ffd361d73df7e72f939e12f9c78cd2306f
Thamitha <thamitharajput@gmail.com>
2023-04-30 00:24:18 +0530

    Add the css and js file

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 const heading=React.createElement(
-    "div",{id:"parent"},
+    "div",{id:"parent"},[
     React.createElement("div",{id:"child1"},[
     React.createElement("h1",{},"I'm H1 tag"),
-    React.createElement("h2",{},"I'm h2 tag")]
-    ));
+    React.createElement("h2",{},"I'm h2 tag")],
+    
+    React.createElement("div",{id:"child2"},[
+        React.createElement("h1",{},"I'm H1 tag"),
+        React.createElement("h2",{},"I'm h2 tag")],
+    )),]);
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
