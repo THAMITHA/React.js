3e3c2772cc8ca1c9129d7254d720e8c355fd5aa5
Thamitha <thamitharajput@gmail.com>
2023-04-29 23:58:40 +0530

    add css file

--- App.js
+++ App.js
@@ -1,3 +1,3 @@
-const heading = React.createElement("h1", {}, "Hello World from React!");
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const heading=React.createElement("h1",{id:"heading",xyz:"abc"},"Helloworld from react");
+const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
