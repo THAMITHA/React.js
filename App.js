2a2a82ff913fdb2c15b9778975136a8827bd6b1a
Thamitha <thamitharajput@gmail.com>
2023-04-30 00:09:18 +0530

    add css file

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 const heading=React.createElement(
-    "div",
-    {id:"parent"},React.createElement("div",{id:"child"},[
+    "div",{id:"parent"},
+    React.createElement("div",{id:"child1"},[
     React.createElement("h1",{},"I'm H1 tag"),
     React.createElement("h2",{},"I'm h2 tag")]
     ));
